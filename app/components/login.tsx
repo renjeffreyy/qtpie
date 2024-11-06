d14dc224573f77629dbb313f81524a5dae8b80a7
Jeffrey Ren <renjeffreyy@gmail.com>
2024-11-06 12:35:12 -0800

    added form for login

---
+++ app/components/login.tsx
@@ -0,0 +1,71 @@
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+
+const formSchema = z.object({
+  email: z.string().email().min(5),
+  password: z.string().min(8,{
+    message: "password must be at least 8 characters"
+  })
+})
+
+
+function onSubmit(values: z.infer<typeof formSchema>) {
+  console.log(values)
+}
+
+export function ProfileForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password:""
+    },
+  })
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
+  )
+
+}
+
+export default function Login() {
+    return (
+      <>
+      <h1 className="">Login</h1>
+      <ProfileForm />
+      </>
+    );
+  }
---
+++ app/routes/account.tsx
@@ -0,0 +1,12 @@
+import Login from "~/components/login";
+import Register from "~/components/register";
+
+export default function Index() {
+    return (
+      <>
+      <h1 className="">QTPie Account</h1>
+      <Login />
+      <Register />
+      </>
+    );
+  }
